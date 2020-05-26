55ebd6b8b0773898946af981483d825a6926e3a6
Daniel Abrao <danielpaladar@gmail.com>
2020-05-26 15:02:34 -0300

    Add student files into my context

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,3 @@ RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 ENTRYPOINT ["gunicorn"  , "-b", ":8080", "main:APP"]
-
-
---
+++ eksIAMSetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+export TRUST="{ \"Version\": \"2012-10-17\", \"Statement\": [ { \"Effect\": \"Allow\", \"Principal\": { \"AWS\": \"arn:aws:iam::${ACCOUNT_ID}:root\" }, \"Action\": \"sts:AssumeRole\" } ] }"
+export aws iam create-role --role-name UdacityFlaskDeployCBKubectlRole --assume-role-policy-document "$TRUST" --output text --query 'Role.Arn'
